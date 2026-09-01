2815487f99827bc16d6aff983e322d314fb77748
stiven <stiven.sana.dev@gmail.com>
2026-08-31 21:23:19 -0500

    Modificaciones del modulo

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-const express = require('express');
-
+import express from 'express';
+import { configDotenv } from 'dotenv';
+configDotenv();
 const app = express();
-
-const MIPUERTO = 3333;
-
-// middleware body-parse
-
-app.use(express.json())
+const port = process.env.MIPUERTO || 3003
 
 app.get("/", (_, res) => {
     res.send('API REST Full con Express');
 });
 
-app.get("/api/aprendices", (_, res) => {
-    res.status(200).json({mensaje:'lista aprendices'})
-});
-
-app.post("/api/aprendices", (req, res) => {
-    const datosAprendiz = req.body
-    const edad = req.body.edad
-     
-    res.status(201).json({mensaje:'crear aprendiz', datos: datosAprendiz, estado: datosAprendiz >=18? 'Eres Mayor de Edad' : 'Eres menor de edad'})
-    
-});
-
-app.put("/api/aprendices/:id_aprendices", (_, res) => {
-    res.status(200).json({mesaje:'Actializar aprendiz'})
-});
-
-app.delete("/api/aprendices/:id_aprendices", (_, res) => {
-    res.status(200).json({mensaje:'Eliminada'})
-});
- 
-app.listen(MIPUERTO, () => {
-    console.log(`Servidor en funcionamiento en el puerto: ${MIPUERTO}`  );
+app.listen(port, () => {
+    console.log(`Servidor en funcionamiento en el puerto: ${port}`  );
 });
